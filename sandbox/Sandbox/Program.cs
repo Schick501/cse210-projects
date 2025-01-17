bd12c9c71d5749f6de906fa265bb896220f25a6e
Schick501 <156933479+Schick501@users.noreply.github.com>
2025-01-16 17:49:17 -0700

    edit sandbox then finish prep 4

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // this is a comment
+        string adjective = GetAdjective();
+        string noun = GetNoun();
+
+        int number = Multiply(3, 4);
+
+        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}.");
+    }
+
+    static int Multiply(int number1, int number2)
+    {
+        int product = number1 * number2;
+        
+        return product;
+    }
+
+
+
+    static string GetAdjective()
+    {
+        List<string> words = new List<string>();
+        words.Add("blue");
+        words.Add("green");
+        words.Add("yellow");
+        words.Add("small");
+
+        string adjactive = words[2];
+
+        return adjactive;
+    }
+
+    static string GetNoun()
+    {
+        string noun = "bird";
+
+        return noun;
     }
 }
